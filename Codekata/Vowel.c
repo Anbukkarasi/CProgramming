2a209b267c9ef0685a91842efe628960976c531b
Anbukkarasi <38747280+Anbukkarasi@users.noreply.github.com>
2018-04-26 12:25:29 +0530

    Create Vowel.c

---
+++ Codekata/Vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ int n;
+ printf("Enter character:");
+ scanf("%c",&n);
+ if(n=='a' || n=='e' || n=='i' || n=='o'|| n=='u')
+ {
+ printf("Vowel");
+ }
+ else
+ {
+ printf("Consonants");
+ }
+ } 

d7425bd15e0e4a8eef5e1b846969af793eeb39f1
Anbukkarasi <38747280+Anbukkarasi@users.noreply.github.com>
2019-06-21 15:05:02 +0530

    Create B35.c

---
+++ B35.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  char a[100];
+  int i,c=0;
+  gets(a);
+  for(i=0;a[i]!='\0';i++)
+  {
+    if(a[i] >= '0' && a[i] <='9')
+    {
+      c = c+1;
+    }
+  }
+  printf("%d",c);
+  return 0;
+}

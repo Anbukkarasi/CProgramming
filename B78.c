e0ae1892b064a05fcb5e7401cc695e2b83c5e3e1
Anbukkarasi <38747280+Anbukkarasi@users.noreply.github.com>
2019-07-17 14:33:20 +0530

    Create B78.c

---
+++ B78.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+
+int main(void) {
+  int n,i,c=0;
+  scanf("%d",&n);
+  if(n%13 == 0)
+    {
+      printf("Yes");
+    }
+    else{
+      printf("Yes");
+    }
+  
+  return 0;
+}
